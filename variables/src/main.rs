0e50b84fbcf630798dde04e8bbd56c0c8d37be93
negeek7 <negeek7@gmail.com>
2025-05-30 18:17:04 +0530

    about const variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,8 @@
 // By default variables are immutable in rust
-
-
 // We can make a variable mutable by using 'mut' keyword before it
+// const is different from let as it is entriely immutable, even with mut keyword
+// We also cannot any result of the function to a const variable but to let
+// const variables are to be type annotated
 
 
 fn main() {
@@ -9,4 +10,9 @@ fn main() {
     println!("{}", x);
     x = 10;
     println!("{}", x);
+
+    // const mut AGE: u32 = 25; // This will not work
+    const AGE: u32 = 25;
+
+    println!("{}", AGE);
 }
