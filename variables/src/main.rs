cef70adf51bb484e88ea37f4bb49583971a75e4c
negeek7 <negeek7@gmail.com>
2025-05-30 18:13:50 +0530

    let mut x

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 // By default variables are immutable in rust
 
+
+// We can make a variable mutable by using 'mut' keyword before it
+
+
 fn main() {
-    let x = 5;
-    println!(x);
+    let mut x = 5;
+    println!("{}", x);
+    x = 10;
+    println!("{}", x);
 }
