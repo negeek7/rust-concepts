c5da927905b33ba1123e6f4b854e54c7db0f0ba0
negeek7 <negeek7@gmail.com>
2025-05-30 18:46:46 +0530

    data types in rust

---
+++ datatypes/src/main.rs
@@ -0,0 +1,33 @@
+// Data Types
+
+// Scaler data types
+    // represent single value
+
+fn main() {
+    // integers
+    // let x: i32 = 98_222; // Decimal
+    // let x: i32 = 0xff; // Hex
+    // let x: i32 = 0o77; // Octal
+    // let x: i32 = 0b1111_0000; // Binary
+    // let c: u8 = b'A'; // Byte
+
+    // println!("{}", c);
+    // // floating-point numbers
+    // let f: f64 = 2.0;
+    // let g: f32 = 3.0;
+
+    //addition
+    let sum: f64 = 11.2 + 54.2;
+    let sub: f64 = 54.00 - 10.00;
+    println!("{}", sub)
+    // booleans
+
+    // character
+}
+
+
+
+// Compound data types
+// fn main() {
+//     println!("Hello, world!");
+// }
