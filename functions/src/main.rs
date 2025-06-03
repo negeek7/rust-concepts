8ee4771ab74af602402503342f40507613e057e9
negeek7 <negeek7@gmail.com>
2025-06-03 20:09:51 +0530

    placeholder for function return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!(my_function(20, 25));
+    println!("{}", my_function(20, 25));
 }
 
-fn my_function(num1: u32, num2: u32) -> u32 {
+fn my_function(num1: i32, num2: i32) -> i32 {
     let sum = num1 + num2;
 
     // return sum;
