41f98825b4f90f924aa98bfd51dca44e0fccd1b3
negeek7 <negeek7@gmail.com>
2025-06-03 20:20:04 +0530

    loop control flow / returning value from loop

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
-    println!("{}", my_function(20, 25));
+    let result = loop_up();
+    println!("Final number is {}", result);
 }
 
 fn my_function(num1: i32, num2: i32) -> i32 {
-    let sum = num1 + num2;
+    num1 + num2 
+}
 
-    // return sum;
 
-    sum // another way of returning in rust
+fn loop_up() -> i32 {
+    let mut counter: i32 = 0;
+
+    let result: i32 = loop {
+        counter += 1;
+        println!("{}", counter);
+        if(counter == 10) {
+            break counter;
+        }
+    };
+
+    result
 }
