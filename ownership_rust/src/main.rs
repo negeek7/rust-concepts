015e32304e03f17dba2fa48370a2e0f1b5fa78bc
negeek7 <negeek7@gmail.com>
2025-06-06 13:36:04 +0530

    move and shallow copy striing

--- ownership_rust/src/main.rs
+++ ownership_rust/src/main.rs
@@ -5,9 +5,15 @@ fn main() {
         // There can only be one owner at a time
         // When the owner goes out of scope the value is dropped  
 
-        {
-            let str = "Hello";
-            println!("{}", str);
-        }
-        println!("{}", str);
-}
+        // {
+        //     let str = "Hello";
+        //     println!("{}", str);
+        // }
+        // println!("{}", str);
+
+
+        let s1 = String::from("hello");
+        let s2 = s1; // Move (not shallow copy, meaning s1/s1's value has been MOVED to s2)
+
+        println!("{}, world!", s1);
+    }
